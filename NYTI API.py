0e33f2b3bcd9ca3aa5ba910abe82aeeee17fd095
mealzhappy <ams2638@columbia.edu>
2023-04-04 14:51:57 -0400

    Update NYTI API.py

--- NYTI API.py
+++ NYTI API.py
@@ -15,7 +15,7 @@ import pandas as pd
 
 from config import *
 
-key = key
+key = 'M8zMmwpJDM3RUq2NdehtkyOjwMxHpZoc'
 
 nyt = NYTAPI(
     key=key,
@@ -59,17 +59,66 @@ all_df = pd.concat([obama_df, trump_df], axis = 0)
 
 
 
-
 """Pre-processing"""
-
 def clean_text(str_in):
     import re
     tmp = re.sub("[^A-Za-z#!']+", " ",str_in).lower().strip()
     return tmp
 
 
+all_df["abstract_clean"] = all_df["abstract"].apply(clean_text)
 
-"""TFIDF"""
+
+def rem_sw(var_in):
+    from nltk.corpus import stopwords
+    sw = stopwords.words("english")
+    tmp = var_in.split()
+    # tmp_ar = list()
+    # for word_t in tmp:
+    #     if word_t not in sw:
+    #         tmp_ar.append(word_t)
+    tmp_ar = [word_t for word_t in tmp if word_t not in sw]
+    tmp_o = ' '.join(tmp_ar)
+    return tmp_o
+
+
+all_df["abstract_sw"] = all_df["abstract_clean"].apply(rem_sw)
+
+
+def stem_fun(txt_in):
+    from nltk.stem import PorterStemmer
+    stem_tmp = PorterStemmer()
+    tmp = [stem_tmp.stem(word) for word in txt_in.split()]
+    tmp = ' '.join(tmp)
+    # tmp = list()
+    # for word in txt_in.split():
+    #     tmp.append(stem_tmp.stem(word))
+    return tmp
+
+all_df["abstract_stem"] = all_df["abstract_sw"].apply(stem_fun)
+
+
+
+
+
+"""Sentiment Analysis"""
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+all_df["vader1"] = [analyzer.polarity_scores(a)["compound"] for a in all_df["abstract"]]
+
+all_df["vader_clean"] = [analyzer.polarity_scores(a)["compound"] for a in all_df["abstract_clean"]]
+
+all_df["vader_sw"] = [analyzer.polarity_scores(a)["compound"] for a in all_df["abstract_sw"]]
+
+all_df["vader_stem"] = [analyzer.polarity_scores(a)["compound"] for a in all_df["abstract_stem"]]
+
+
+
+
+"""
+TFIDF
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 xform = TfidfVectorizer(ngram_range=(1, 1))
@@ -77,5 +126,5 @@ xform_df_obama = pd.DataFrame(xform.fit_transform(obama_df['abstract']).toarray(
 xform_df_obama.columns = xform.get_feature_names()
 
 xform_df_trump = pd.DataFrame(xform.fit_transform(trump_df['abstract']).toarray())
-xform_df_trump.columns = xform.get_feature_names()
+xform_df_trump.columns = xform.get_feature_names()"""
 
